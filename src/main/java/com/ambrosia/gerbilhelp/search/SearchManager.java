f67418af08a0f74cf786d57ffb1229e4bc30ab34
Poisoned-Honey <poisonedhoney13@gmail.com>
2023-08-07 01:01:34 -0700

    Clean up garbage

--- src/main/java/com/ambrosia/gerbilhelp/search/SearchManager.java
+++
@@ -1,7 +0,0 @@
-package com.ambrosia.gerbilhelp.search;
-
-import com.ambrosia.gerbilhelp.internal.RequestManager;
-import com.ambrosia.gerbilhelp.internal.RequestResult;
-
-public class SearchManager {
-}
